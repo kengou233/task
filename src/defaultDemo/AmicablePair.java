cca0244969162c0916c6b69a6549ae2af21184a8
kengou <1974356779@qq.com>
2019-06-13 12:47:19 +0800

    修改

--- src/defaultDemo/AmicablePair.java
+++ src/defaultDemo/AmicablePair.java
@@ -1,8 +1,6 @@
 package defaultDemo;
-
 /**
- * 相亲数 一个数的因子和等于另一个数 另一个数的因子和等于这个数(手动滑稽)
- * 
+ * 相亲数：彼此除本身以外的因数和 等于 彼此
  * @author 创建人：灰凉
  * @date 创建时间：2019年6月13日下午12:02:33
  */
@@ -12,14 +10,15 @@ public class AmicablePair {
 		// 对了 先求因数 嘿嘿
 		for (int i = 1; i < 9999; i++) {
 			int n = factor(i);
-			if (i > n && factor(n) == i)//如果前一个数大于它的因子和 并且 它的因子和的因子和等于它本身
+			if (i > n && factor(n) == i)// 如果前一个数大于它的因子和 并且 它的因子和的因子和等于它本身
 				System.out.println(i + "\t" + n);
 		}
 	}
-/**
- * 将该数字除本身以外所有因数相加
- * @return
- */
+	/**
+	 * 将该数字除本身以外所有因数相加
+	 * 
+	 * @return
+	 */
 	public static int factor(int i) {
 		int sum = 0;
 		for (int j = 1; j < i; j++) {

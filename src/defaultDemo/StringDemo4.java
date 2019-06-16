bd8a55e10b3ef5de6f1c51e98754c5fabe3a37a7
kengou <1974356779@qq.com>
2019-06-16 10:57:20 +0800

    增加

---
+++ src/defaultDemo/StringDemo4.java
@@ -0,0 +1,16 @@
+package defaultDemo;
+
+/**
+ * 使用字符串生成器 追加1~10十个数字
+ * @author	 创建人：灰凉
+ * @date   创建时间：2019年6月16日上午10:28:43
+ */
+public class StringDemo4 {
+	public static void main(String[] args) {
+		StringBuffer bf =new StringBuffer("没想到吧 嘿嘿");
+		for(int i=1;i<11;i++) {
+			bf.append(i);
+		}
+		System.out.println(bf);
+	}
+}
